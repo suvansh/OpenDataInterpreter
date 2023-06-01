c95f57cae2abe9f0c66f997b8dee699482c146b1
Suvansh Sanjeev <suvansh.sanjeev@gmail.com>
2023-05-31 23:41:29 -0700

    attempted use of langchain pydantic parser – doesn't work reliably due to quoting issues within code in JSON object

--- opendi-backend/app.py
+++ opendi-backend/app.py
@@ -18,6 +18,7 @@ from langchain.chains.chat_vector_db.prompts import CONDENSE_QUESTION_PROMPT
 from langchain.chains import LLMChain
 from langchain.chains.conversational_retrieval.base import _get_chat_history
 from langchain import PromptTemplate
+from langchain.prompts import ChatPromptTemplate
 
 import logging
 logger = logging.getLogger(__name__)
@@ -31,9 +32,12 @@ file_handler.setFormatter(formatter)
 # Add the file handler to the logger
 logger.addHandler(file_handler)
 
-def log(msg, allowLogging):
+def log(msg, allowLogging, error=False):
     if not allowLogging: return
-    logger.info(msg)
+    if not error:
+        logger.info(msg)
+    else:
+        logger.exception("An error occurred: %s", msg)
     if debug:
         print(msg)
 
@@ -50,24 +54,27 @@ app.add_middleware(
 
 async def process_file(headers_info, query, model, lang, allowLogging, df=None):
     if lang == "sql":  # SQL mode – no dataframe
-        template = get_sql_prompt(headers_info)
+        template, parser = get_sql_prompt(headers_info)
     else:  # Python mode – use dataframe
-        template = get_python_prompt(df, headers_info)
-    prompt = PromptTemplate(template=template, input_variables=["query"])
+        template, parser = get_python_prompt(df, headers_info)
+    messages = template.format_messages(query=query)
+    log(messages, allowLogging)
     llm_chains = {
-        "GPT-3.5": LLMChain(prompt=prompt, llm=ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.9), verbose=debug),
-        "GPT-4": LLMChain(prompt=prompt, llm=ChatOpenAI(model_name="gpt-4", temperature=0.9), verbose=debug),
+        "GPT-3.5": ChatOpenAI(model_name="gpt-3.5-turbo", temperature=0.9),
+        "GPT-4": ChatOpenAI(model_name="gpt-4", temperature=0.9)
     }
-    response = await llm_chains[model].apredict(query=query)
-    log(f"LLM Response: {response}", allowLogging)
+    response = await llm_chains[model].apredict_messages(messages)
+    log(f"LLM Response: {response.content}", allowLogging)
+    try:
+        output = parser.parse(response.content)
+    except Exception as e:
+        log(e, allowLogging, error=True)
+        return {"answer": "[Error] Got an error parsing the LLM response.", "images": [], "lang": lang, "code": "Code could not be parsed from LLM response." }
+    log(f"LLM Response: {output}", allowLogging)
     if lang == "sql":  # SQL mode – return code
-        return {"lang": lang, "code": response}
-    else:
-        output = parse_LLM_response(response)
-        if 'error' in output:
-            log("LLM error: " + output['error'], allowLogging)
-            return {"answer": "[Error] Got an error parsing the LLM response.", "images": [], "lang": lang, "code": "Code could not be parsed." }
-        code, out_variable, img_paths_str = output["code"], output["out_variable"], output["img_paths_str"]
+        return {"lang": lang, "code": output.code}
+    else:   
+        code, out_variable, img_paths = output.code, output.out_variable, output.img_paths
         if not check_code(code):
             log("Code check failed.", allowLogging)
             return {"answer": "[Error] The generated code was not safe to run.", "images": [], "lang": lang, "code": code }
@@ -75,14 +82,9 @@ async def process_file(headers_info, query, model, lang, allowLogging, df=None):
             new_globals = run_code(df, code)
             # Get output
             out_str = eval(out_variable, new_globals)
-            img_paths = eval(img_paths_str, new_globals)
         except Exception as e:
-            logger.exception("An error occurred: %s", e)
+            log(e, allowLogging, error=True)
             return {"answer": "[Error] Got an error while running the code.", "images": [], "lang": lang, "code": code }
-        
-        if len(img_paths) == 1 and not img_paths[0].endswith("png"):
-            img_paths[0] = new_globals.get(img_paths[0], img_paths[0])
-        img_paths = [img_path for img_path in img_paths if img_path]
         
         log(f"Final output: {out_str}", allowLogging)
         image_urls = await upload_images(img_paths)
--- opendi-backend/prompts.py
+++ opendi-backend/prompts.py
@@ -1,51 +1,54 @@
-def get_plotly_prompt(headers_info):
-    prompt = f"""You are a helpful expert data analyst. Your task is to write JavaScript code that uses plotly to generate graphs in response to questions about data, and place these graphs in a div with id 'graphDiv'.
-    The data is in a variable called 'csvData' that is an array of objects. Each object represents a row in the data. The keys of each object will be the column names of the data. There are {len(headers_info)} columns.
-    The columns in the data (and descriptions of some of them) are:
-    """
-    for col_name, col_info in headers_info.items():
-        prompt += f"- {col_name}: {col_info}\n"
-    prompt += f"""\nOnly write JavaScript code to be executed to generate the Plotly graphs, with no other output. If you include other output it will cause an error. Do not import other libraries.
-    Here are some examples of questions you might be asked. The examples use made up columns, but you should use the columns in the data as described above.
-    """
-    prompt += """Question: Make a bar chart of the number of people in each state.
-    JavaScript code: function plotData(data) { Plotly.newPlot('graphDiv', [{x: csvData.map(row => row['state']), y: csvData.map(row => row['population']), type: 'bar'}]); }
-    Question: Make a scatterplot of the tuition of the school vs the number of students.
-    JavaScript code: function plotData(data) { Plotly.newPlot('graphDiv', [{x: csvData.map(row => row['tuition']), y: csvData.map(row => row['students']), type: 'scatter'}]); }
-    """
-    
-    return prompt
+from langchain.output_parsers import ResponseSchema
+from langchain.output_parsers import PydanticOutputParser, StructuredOutputParser
+from langchain.prompts import ChatPromptTemplate
+from pydantic import BaseModel, Field, validator
+from typing import List
+
+class PythonOutput(BaseModel):
+    code: str = Field(..., description="Python code to answer the question using data in the Pandas dataframe `df`.")
+    out_variable: str = Field(..., description="The name of a Python variable resulting from executing the above code, that contains human-readable text answering the user's question.")
+    img_paths: List[str] = Field(..., description="A comma-separated Python list of string paths to images saved upon executing the Python code.")
+
+class SQLOutput(BaseModel):
+    code: str = Field(..., description="SQL code to answer the question using data in the SQL table `mytable`.")
+
 
 def get_sql_prompt(headers_info):
-    prompt = f"""You are a helpful expert data analyst. Your task is to write SQL code for the table called `mytable` to answer questions about the data.
-    The data is in a variable called 'csvData' that is an array of objects. Each object represents a row in the data. The keys of each object will be the column names of the data. There are {len(headers_info)} columns.
-    The columns in the data (and descriptions of some of them) are:
+    parser = PydanticOutputParser(pydantic_object=SQLOutput)
+    format_instructions = parser.get_format_instructions()
+
+    template = f"""You are a helpful expert data analyst. Your task is to write SQL code for the table called `mytable` to answer queries about the data.
+    There are {len(headers_info)} columns. The columns in the data (and descriptions of some of them) are:
     """
     for col_name, col_info in headers_info.items():
-        prompt += f"- {col_name}: {col_info}\n"
-    prompt += f"""\nOnly write SQL code to be executed to answer the question, with no other output. If you include other output it will cause an error.
-    Here are some examples of questions you might be asked. The examples use made up columns, but you should use the columns in the data as described above.
-    """
-    prompt += """Question: How many people are in each state?
-    SQL code: SELECT state, COUNT(*) FROM mytable GROUP BY state;
-    Question: What is the average tuition of schools in each city, ordered by average tuition?
-    SQL code: SELECT city, AVG(tuition) as avg FROM mytable GROUP BY city ORDER BY avg;
-    Question: {query}
-    SQL code: """
+        template += f"- {col_name}: {col_info}\n"
+    template += """\nOnly write SQL code to be executed to answer the question, with no other output. If you include other output it will cause an error.
+    If column names have spaces or special characters, you must use backticks to escape them, e.g. `Height (inches)`.
     
-    return prompt
+    {format_instructions}
+    
+    Query: {query}
+    """
+    
+    return ChatPromptTemplate.from_template(
+        template,
+        partial_variables={"format_instructions": format_instructions}
+    ), parser
 
 def get_python_prompt(df, headers_info):
-    prompt = f"""You are a helpful data analysis expert. You have access to a pandas DataFrame named `df` containing data that you are to answer questions about.
+    parser = PydanticOutputParser(pydantic_object=PythonOutput)
+    format_instructions = parser.get_format_instructions()
+
+    template = f"""You are a helpful data analysis expert. You have access to a pandas DataFrame named `df` containing data that you are to answer questions about.
 The columns in the data (and descriptions of some of them) are:
 """
     for col_name, col_info in headers_info.items():
-        prompt += f"- {col_name}: {col_info}\n"
-    prompt += """
+        template += f"- {col_name}: {col_info}\n"
+    template += """
 Here is the output of `df.head()`. You can use it to understand the data format:
 """
-    prompt += df[list(headers_info.keys())].head().to_string(index=False)
-    prompt += """
+    template += df[list(headers_info.keys())].head().to_string(index=False)
+    template += """
 
 Be sure to use only columns specified above.
 You can use the following libraries (with their respective aliases if specified) without importing them.
@@ -63,25 +66,45 @@ You are allowed to use these Python built-in functions:
 
 When generating an image, do not use `plt.show()`. Instead, save the image with `plt.savefig()` to a unique path. When outputting a dataframe, use the `to_html(index=False)` method to convert it to HTML.
 
-Your output should be newline-separated with three lines.
+For the query, provide the following information:
 
-CODE: a string containing the Python code to execute in order to answer the question. Be mindful of quote types and escaping. If you use f-strings, you must enclose them in triple quotes to avoid escaping issues. This code may merely be assigning a string to out_variable, or it may be more complex.
-OUT_VARIABLE: the name of a variable containing the output, in HTML format, to return to the user. This output should be helpful, friendly, and in complete sentences. It should answer the question. Be sure to include any relevant tables in the output.
-IMG_PATHS: a possibly empty list of strings containing the paths to files that requested images were saved to, so they can be shown to the user.
-Please generate code and output based on the provided DataFrames and the allowed libraries and built-ins. Remember to save any generated images to a unique path and return the path in the "IMG_PATHS" list. Do not provide any output other than this.
+code: Python code to answer the question and generate a response to the user. Be mindful of quote types and escaping. If you use f-strings, you must enclose them in triple single-quotes (NOT double-quotes) to avoid escaping issues. This code may merely be assigning a string to out_variable, or it may be more complex. If the user's query is not answerable using the dataframe, return an empty code block.
 
-If the user's query is not answerable using the dataframe, say only "ERROR: Sorry, I don't know how to answer that question.".
-If you want to reply with a string, assign it in appropriate HTML to a variable in the CODE section and put the name of the variable in OUT_VARIABLE. Do NOT put the string itself in OUT_VARIABLE.
+out_variable: The name of a variable that contains the output, in HTML format, to return to the user after running the code. This output should be helpful, friendly, and in complete sentences. It should answer the question. Be sure to include any relevant tables in the output.
 
-Remember to use triple quotes (\"\"\") for f-strings to avoid escaping issues and accidentally closing the string early.
+img_paths: Generate a comma-separated Python list of paths to images containing outputs saved upon executing the Python code.
 
-Below is an example using made-up columns. Remember that you are to use the columns given above.
+Please generate code and output based on the provided DataFrames and the allowed libraries and built-ins. Remember to save any generated images or files to a unique path and return the path in the "file_paths" list. Do not provide any output other than this.
+If you want to reply with a string, assign it in appropriate HTML to a variable in the code section and put the name of the variable in out_variable.
+In your code, only use single-quotes for strings, not double-quotes. If you need to use double-quotes, enclose the string in triple single-quotes.
+When escaping characters, use double-backslashes, since the code string will be parsed twice.
 
-Query: Graph Matt Ryan's passing yards for each season of his career.
-CODE: matt_ryan_stats = df[df['player_name'] == 'Matt Ryan']\nmatt_ryan_seasons = matt_ryan_stats.groupby('season')['passing_yards'].sum().reset_index()\n\nplt.figure(figsize=(10, 6))\nsns.barplot(x='season', y='passing_yards', data=matt_ryan_seasons)\nplt.title('Matt Ryan\'s Passing Yards per Season')\nplt.xlabel('Season')\nplt.ylabel('Passing Yards')\nimg_path = 'matt_ryan_passing_yards.png'\nplt.savefig(img_path)\nout_variable="Here is a graph of Matt Ryan's passing yards for each season of his career."
-OUT_VARIABLE: out_variable
-IMG_PATHS: ['matt_ryan_passing_yards.png']
+{format_instructions}
+
 Query: {query}
-    """
+"""
     
-    return prompt
+    return ChatPromptTemplate.from_template(
+        template,
+        partial_variables={"format_instructions": format_instructions}
+    ), parser
+
+
+# TODO get plotly viz working for in-browser viz
+# def get_plotly_prompt(headers_info):
+#     prompt = f"""You are a helpful expert data analyst. Your task is to write JavaScript code that uses plotly to generate graphs in response to questions about data, and place these graphs in a div with id 'graphDiv'.
+#     The data is in a variable called 'csvData' that is an array of objects. Each object represents a row in the data. The keys of each object will be the column names of the data. There are {len(headers_info)} columns.
+#     The columns in the data (and descriptions of some of them) are:
+#     """
+#     for col_name, col_info in headers_info.items():
+#         prompt += f"- {col_name}: {col_info}\n"
+#     prompt += f"""\nOnly write JavaScript code to be executed to generate the Plotly graphs, with no other output. If you include other output it will cause an error. Do not import other libraries.
+#     Here are some examples of questions you might be asked. The examples use made up columns, but you should use the columns in the data as described above.
+#     """
+#     prompt += """Question: Make a bar chart of the number of people in each state.
+#     JavaScript code: function plotData(data) { Plotly.newPlot('graphDiv', [{x: csvData.map(row => row['state']), y: csvData.map(row => row['population']), type: 'bar'}]); }
+#     Question: Make a scatterplot of the tuition of the school vs the number of students.
+#     JavaScript code: function plotData(data) { Plotly.newPlot('graphDiv', [{x: csvData.map(row => row['tuition']), y: csvData.map(row => row['students']), type: 'scatter'}]); }
+#     """
+    
+#     return prompt
